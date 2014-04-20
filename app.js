43309922a21e4141a71fc67672c4efacac5c9ade
nitiraj.rathore <nitiraj.rathore@gmail.com>
2014-04-20 10:25:29 +0530

    requirejs with folder structure of the project and relative paths.

--- app.js
+++
@@ -1,6 +0,0 @@
-/**
- * Created by nitiraj on 20/4/14.
- */
-require.config({
-    baseUrl: "/TestReqireJs"
-});
--- js/tests/test.js
+++ js/tests/test.js
@@ -1,6 +1,6 @@
 /**
  * Created by nitiraj on 20/4/14.
  */
-require(["Log"], function(logger){
+require(["../app/Log"], function(logger){
     logger.log("testing....");
 });
